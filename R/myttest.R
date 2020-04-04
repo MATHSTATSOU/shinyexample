338f99cd58f221b475638ff91c1d972dbba5b560
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-04-04 17:57:40 -0500

    New function added t test acting as constructor

---
+++ R/myttest.R
@@ -0,0 +1,36 @@
+#' A constructor function for t tests
+#'
+#' This is an example of what can be done with S3 OOP
+#'
+#' The constructor function makes a list that can be worked and further processed with an appropriate method(s)
+#'
+#' @param x vector of data
+#' @param y vector of data
+#' @param paired TRUE or FALSE, FALSE by default
+#' @param alpha alpha level between 0,1
+#'
+#' @return list containing the data, and t test object
+#' @export
+#'
+#' @examples
+#' myttest(x=rnorm(30,10,12), y=rnorm(40, 7, 10))
+myttest<-function(x,y,paired=FALSE, alpha=0.05){
+  if(paired=="FALSE"){
+    vt=var.test(x,y)
+
+  if(vt$p.value>0.05){
+    tt<-t.test(x,y,var.equal = TRUE, conf.level=1-alpha)
+  }
+  else{
+    tt<-t.test(x,y,var.equal = FALSE, conf.level=1-alpha)
+  }
+  }
+  else{
+    stopifnot(length(x)==length(y))
+    tt<-t.test(x,y,paired=TRUE,conf.level=1-alpha)
+  }
+  df = data.frame(x=x,y=y)
+  lst=list(ttest=tt, df=df)
+ `mytt`<-class(lst)
+ lst
+}
